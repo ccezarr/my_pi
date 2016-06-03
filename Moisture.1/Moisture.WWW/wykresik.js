33ae9fea6cc947f2cfa59f78738f79d6c146d136
Cezar Pi <jacek.kutyla@gmail.com>
2016-06-03 22:55:15 +0100

    Working on database

--- Moisture.1/Moisture.WWW/wykresik.js
+++ Moisture.1/Moisture.WWW/wykresik.js
@@ -1,3 +1,55 @@
+var express = require ("express");
+var mysql = require("mysql");
+var connection = mysql.createConnection ({
+	host : 'localhost',
+	user : 'monitor',
+	password : 'password',
+	database : 'pomiary'
+});
+
+var globalResponse;
+var app = express();
+
+connection.connect(function(err){
+if(!err) {
+	console.log ("Database is connected ... nn");
+} else {
+	console.log ("Error connecting database ... nn");
+	console.log(err);
+}
+});
+
+app.get("/", function(req,res){
+globalResponse=res;
+connection.query('SELECT * from moisture LIMIT 100', function (err,rows,fields) {
+globalResponse.writeHeader(200, {'Content-Type': 'text/html'});  
+if (!err)
+{
+	console.log('The solution is: ',rows);
+	globalResponse.write('The solution is:' + '<br/>');
+	for (var i = 0; i< rows.length; i++)
+	{
+		var row = rows[i];
+		globalResponse.write(row.tdate.toString());
+		globalResponse.write(row.ttime.toString());
+		globalResponse.write(row.moisture.toString());
+
+		globalResponse.write('<br/>');
+	}
+}
+else
+{
+	console.log('Error while performing query.');
+}
+globalResponse.end(); 
+});
+});
+
+app.listen(8000);
+
+
+/*
+
 var http = require('http'),
 //	window = require('chart.js'),
     fs = require('fs');
@@ -21,3 +73,6 @@ var http = require('http'),
     	}).listen(8000);
 
 
+
+
+*/
