8cb839a24e7cb98d41b9b5235a9baad6c0a1c667
Cezar Pi <jacek.kutyla@gmail.com>
2016-06-03 21:50:24 +0100

    Adding Moisture

---
+++ Moisture.1/Moisture.WWW/wykresik.js
@@ -0,0 +1,22 @@
+var http = require('http'),
+    fs = require('fs');
+
+    http.createServer(
+	function(request, response) 
+	{ 
+		fs.readFile('wykresik.html', 'utf-8',  function (err, html) 
+		{
+    			if (err) 
+			{
+        			throw err; 
+    			}        
+        		response.writeHeader(200, {'Content-Type': 'text/html'});  
+        		response.write(html);
+			console.log(err);
+			console.log ('file loaded.');
+			console.log(html.toString('utf-8'));
+        		response.end();  
+		})
+    	}).listen(8000);
+
+
