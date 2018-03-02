40416a483522960dc165205ba32ab3beba0264d2
root <root@raspberrypi>
2018-03-02 18:36:59 +0000

    CCTV

---
+++ PogadankaCCTV/cctv1.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
+
+while True:
+    i=GPIO.input(11)
+    if i==0:                 #When output from motion sensor is LOW
+        print ("No intruders")
+        time.sleep(0.5)
+    elif i==1:               #When output from motion sensor is HIGH
+        print ("Intruder detected")
+        time.sleep(0.5)
